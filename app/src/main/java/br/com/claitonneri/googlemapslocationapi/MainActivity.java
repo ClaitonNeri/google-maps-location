55c1e60049e3ce570ddfea7ed7ec50ddb064a577
ClaitonND <claitondidone@gmail.com>
2018-08-15 22:11:36 -0300

    Projeto Inicial

--- app/src/main/java/br/com/claitonneri/googlemapslocationapi/MainActivity.java
+++ app/src/main/java/br/com/claitonneri/googlemapslocationapi/MainActivity.java
@@ -1,13 +1,124 @@
 package br.com.claitonneri.googlemapslocationapi;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txt_location;
+    Button botaoIniciar;
+    Button botaoParar;
+
+    private static final int REQUEST_CODE = 1000;
+
+    FusedLocationProviderClient fusedLocationProviderClient;
+    LocationRequest locationRequest;
+    LocationCallback locationCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Iniciar a View
+        txt_location = findViewById(R.id.txt_location);
+        botaoIniciar = findViewById(R.id.btn_iniciar_atualiz);
+        botaoParar = findViewById(R.id.btn_parar_atualiz);
+
+        // Checando as permissoes em tempo de execucao
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+        } else {
+            // Se a permissao for autorizada
+            buildLocationRequest();
+            buildLocationCallBack();
+
+            // Criar o FusedProviderClient
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+            // Implementar evento para os botoes
+            // Botao Iniciar
+            botaoIniciar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+                        return;
+                    }
+
+                    fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+
+                    // Trocar o estado do botao Parar
+                    botaoIniciar.setEnabled(!botaoIniciar.isEnabled());
+                    botaoParar.setEnabled(!botaoParar.isEnabled());
+                }
+            });
+
+            // Botao Parar
+            botaoParar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+                        return;
+                    }
+
+                    fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+
+                    // Trocar o estado do botao Parar
+                    botaoIniciar.setEnabled(!botaoIniciar.isEnabled());
+                    botaoParar.setEnabled(!botaoParar.isEnabled());
+                }
+            });
+        }
+    }
+
+    private void buildLocationRequest() {
+        locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(3000);
+        locationRequest.setSmallestDisplacement(10);
+    }
+
+    private void buildLocationCallBack() {
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                for (Location location : locationResult.getLocations()) {
+                    txt_location.setText(String.valueOf(location.getLatitude()) + "/" + String.valueOf(location.getLongitude()));
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_CODE:
+                if (grantResults.length > 0) {
+                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    } else if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+
+                    }
+                }
+        }
     }
 }
